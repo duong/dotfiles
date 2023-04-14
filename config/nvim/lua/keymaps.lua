62e3d0ca21934611d2a4f0244a945796929c39d5
Duong Dinh <dd.dinh11@gmail.com>
2023-04-14 16:40:50 +1000

    feat: move config

--- config/nvim/lua/keymaps.lua
+++ config/nvim/lua/keymaps.lua
@@ -62,6 +62,7 @@ keymap("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
 -- Telescope
 keymap("n", "<leader>ff", ":Telescope find_files<CR>", opts)
 keymap("n", "<leader>ft", ":Telescope live_grep<CR>", opts)
+keymap("n", "<leader>fT", ":Telescope live_grep --hidden --no-ignore<CR>", opts)
 keymap("n", "<leader>fp", ":Telescope projects<CR>", opts)
 keymap("n", "<leader>fb", ":Telescope buffers<CR>", opts)
 
