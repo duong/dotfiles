1506f0eef1aced09595a18ff98e7843cf50c9522
Duong Dinh <dd.dinh11@gmail.com>
2023-10-04 13:47:09 +1100

    feat: hide cmdline when not in use

--- config/kickstart.nvim/init.lua
+++ config/kickstart.nvim/init.lua
@@ -280,6 +280,9 @@ vim.o.completeopt = 'menuone,noselect'
 -- NOTE: You should make sure your terminal supports this
 vim.o.termguicolors = true
 
+-- Hide cmdline when not in use https://www.reddit.com/r/neovim/comments/xb0hs1/is_it_possible_to_hide_command_line_when_it_is/
+vim.o.cmdheight = 0
+
 -- [[ Basic Keymaps ]]
 
 -- Keymaps for better default experience
