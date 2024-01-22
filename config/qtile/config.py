36e1c975163e86faf62638bdbb4977e90b1a5bda
Duong Dinh <dd.dinh11@gmail.com>
2024-01-22 11:48:00 +1100

    fix: bar on multiple screens

--- config/qtile/config.py
+++ config/qtile/config.py
@@ -208,6 +208,82 @@ screens = [
             background=colors["base"],
         ),
     ),
+    Screen(
+        bottom=bar.Bar(
+            [
+                # Left side
+                widget.GroupBox(
+                    active=colors["overlay1"],
+                    background=theme["lavender"],
+                    other_screen_border=theme["lavender"],
+                    other_current_screen_border=theme["lavender"],
+                    this_screen_border=theme["lavender"],
+                    this_current_screen_border=theme["lavender"],
+                    inactive=theme["mantle"],
+                    block_highlight_text_color=theme["text"],
+                    fontsize=15,
+                    padding_x=15,
+                    font=font,
+                    disable_drag=True,
+                ),
+                widget.TextBox(
+                    text="",
+                    fontsize=35,
+                    foreground=theme["lavender"],
+                    background=colors["surface0"],
+                    padding=0,
+                ),
+                widget.Spacer(length=20),
+                widget.WindowName(
+                    foreground=colors["text"],
+                    fontsize=18,
+                    font=font,
+                ),
+                widget.Spacer(length=20),
+                # Right side
+                widget.TextBox(
+                    text="",
+                    fontsize=35,
+                    foreground=colors["surface1"],
+                    background=colors["base"],
+                    padding=0,
+                ),
+                widget.Spacer(length=25, background=colors["surface1"]),
+                widget.TextBox(
+                    text="",
+                    fontsize=35,
+                    foreground=colors["surface0"],
+                    background=colors["surface1"],
+                    padding=0,
+                ),
+                widget.PulseVolume(
+                    background=colors["surface0"],
+                    foreground=colors["text"],
+                    font=font,
+                    fontsize=20,
+                    fmt="墳 {} ",
+                    mouse_callbacks={"Button3": lazy.spawn("pavucontrol -t 4")},
+                ),
+                widget.TextBox(
+                    text="",
+                    fontsize=35,
+                    foreground=colors["mantle"],
+                    background=colors["surface0"],
+                    padding=0,
+                ),
+                widget.Clock(
+                    background=colors["mantle"],
+                    foreground=colors["text"],
+                    format=" %d/%m/%Y  %H:%M ",
+                    font=font,
+                    fontsize=20,
+                    padding=10,
+                ),
+            ],
+            size=35,
+            background=colors["base"],
+        ),
+    ),
 ]
 
 # Drag floating layouts.
