7731b19eb1aa46684086cff20b9e0348215259d6
Duong Dinh <dd.dinh11@gmail.com>
2024-01-29 15:47:06 +1100

    fix: disable virtual_text for nvim-lint eslint_d

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -86,38 +86,6 @@ require('lazy').setup({
         changedelete = { hl = "GitSignsChange", text = "┃", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
         untracked = { hl = "GitSignsUntracked", text = "┃", numhl = "GitSignsUntrackedNr", linehl = "GitSignsUntrackedLn" },
       },
-      signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-      numhl = false,     -- Toggle with `:Gitsigns toggle_numhl`
-      linehl = false,    -- Toggle with `:Gitsigns toggle_linehl`
-      word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
-      watch_gitdir = {
-        interval = 1000,
-        follow_files = true,
-      },
-      attach_to_untracked = true,
-      current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
-      current_line_blame_opts = {
-        virt_text = true,
-        virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-        delay = 1000,
-        ignore_whitespace = false,
-      },
-      current_line_blame_formatter = "<author>, <author_time>, <abbrev_sha> -> <summary>",
-      sign_priority = 6,
-      update_debounce = 100,
-      status_formatter = nil, -- Use default
-      max_file_length = 40000,
-      preview_config = {
-        -- Options passed to nvim_open_win
-        border = "single",
-        style = "minimal",
-        relative = "cursor",
-        row = 0,
-        col = 1,
-      },
-      yadm = {
-        enable = false,
-      },
       on_attach = function(bufnr)
         vim.keymap.set('n', '<leader>hp', require('gitsigns').preview_hunk, { buffer = bufnr, desc = 'Preview git hunk' })
 
@@ -188,14 +156,6 @@ require('lazy').setup({
     config = function()
       -- Your config will go here
     end
-  },
-  {
-    "jay-babu/mason-null-ls.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-      "williamboman/mason.nvim",
-      "nvimtools/none-ls.nvim",
-    },
   },
   -- Fuzzy Finder (files, lsp, etc)
   {
@@ -754,18 +714,9 @@ mason_lspconfig.setup_handlers {
   -- For example, a handler override for the `tsserver`:
   -- ["tsserver"] = function()
   --   require("lspconfig").tsserver.setup {
-  --     commands = {
-  --       OrganizeImports = {
-  --         function()
-  --           vim.lsp.buf.execute_command({
-  --             command = "_typescript.organizeImports",
-  --             arguments = { vim.api.nvim_buf_get_name(0) },
-  --             title = ""
-  --           })
-  --         end,
-  --         description = "Organize Imports"
-  --       }
-  --     }
+  --     handlers = {
+  --       ["textDocument/publishDiagnostics"] = function() end,
+  --     },
   --   }
   -- end
 }
@@ -855,12 +806,27 @@ lint.linters_by_ft = {
 
 vim.api.nvim_create_autocmd({ "InsertLeave", "BufWritePost" }, {
   callback = function()
+    -- Disable virtual_text for eslint_d
+    local ns = require("lint").get_namespace("eslint_d")
+    vim.diagnostic.config({ virtual_text = false }, ns)
+
+    -- Enable linting
     local lint_status, lint = pcall(require, "lint")
     if lint_status then
       lint.try_lint()
     end
   end,
 })
+
+-- Disable all virtual text lsp + lint
+-- vim.diagnostic.config({virtual_text = false})
+
+-- disable all lsp virtual_text
+-- vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+--   vim.lsp.diagnostic.on_publish_diagnostics, {
+--     virtual_text = false
+--   }
+-- )
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
