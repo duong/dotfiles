d1e710f2c032a3a8084bd5f1a14a71ead2c900d0
Duong Dinh <dd.dinh11@gmail.com>
2024-01-30 11:51:09 +1100

    feat: add suggested config for noice

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -852,6 +852,26 @@ require("catppuccin").setup({
   }
 })
 
+-- [[ Configure noice ]]
+require("noice").setup({
+  lsp = {
+    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+    override = {
+      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+      ["vim.lsp.util.stylize_markdown"] = true,
+      ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+    },
+  },
+  -- you can enable a preset for easier configuration
+  presets = {
+    bottom_search = true,         -- use a classic bottom cmdline for search
+    command_palette = true,       -- position the cmdline and popupmenu together
+    long_message_to_split = true, -- long messages will be sent to a split
+    inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+    lsp_doc_border = false,       -- add a border to hover docs and signature help
+  },
+})
+
 -- [[ Configure nvim-lint ]] https://www.reddit.com/r/neovim/comments/15pj1oi/using_nvimlint_as_a_nullls_alternative_for_linters/
 -- local lint = require("lint")
 -- lint.linters_by_ft = {
