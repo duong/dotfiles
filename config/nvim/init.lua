54ccb9e38b6bc19e9e7fe1fd20320b48d7195935
Duong Dinh <dd.dinh11@gmail.com>
2024-01-30 13:26:35 +1100

    fix: config for ufo

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -793,7 +793,21 @@ vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to de
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 
-require('ufo').setup()
+vim.keymap.set('n', 'zR', require('ufo').openAllFolds, { desc = "Open all folds" })
+vim.keymap.set('n', 'zm', require('ufo').closeAllFolds, { desc = "Close all folds" })
+vim.keymap.set('n', 'zK', function()
+  local winid = require('ufo').peekFoldedLinesUnderCursor()
+  if not winid then
+    vim.lsp.buf.hover()
+  end
+end, { desc = "Peek Fold" })
+
+require('ufo').setup({
+  provider_selector = function()
+    return { 'lsp', 'indent' }
+  end
+
+})
 
 -- [[ Configure nvim-cmp ]]
 -- See `:help cmp`
