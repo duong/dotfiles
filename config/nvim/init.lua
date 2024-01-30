410281607ea0ce520793256fe5b922d9198733a6
Duong Dinh <dd.dinh11@gmail.com>
2024-01-30 13:28:32 +1100

    fix: unset foldcolumn in ufo

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -788,7 +788,7 @@ mason_lspconfig.setup_handlers {
 }
 
 -- Configure lspconfig for folding with nvim-ufo
-vim.o.foldcolumn = '1' -- '0' is not bad
+vim.o.foldcolumn = '0' -- '0' is not bad
 vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
