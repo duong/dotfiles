65452a1600a0f32860a513917c9ba537252c8df3
Duong Dinh <dd.dinh11@gmail.com>
2024-01-31 14:44:56 +1100

    feat: add eslint_d to null ls, but its disabled by default

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -907,13 +907,13 @@ local null_ls = require 'null-ls'
 null_ls.setup {
   sources = {
     null_ls.builtins.formatting.stylua,
-    -- null_ls.builtins.diagnostics.eslint,
+    -- null_ls.builtins.diagnostics.eslint_d,
     null_ls.builtins.completion.spell,
     null_ls.builtins.formatting.prettierd,
   },
 }
 require('mason-null-ls').setup {
-  ensure_installed = { 'stylua', 'prettierd' },
+  ensure_installed = { 'eslint_d', 'stylua', 'prettierd' },
   automatic_installation = true,
 }
 
