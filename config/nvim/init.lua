bcc884cae6b6b6acf81252f3a99915be396b9d20
Duong Dinh <dd.dinh11@gmail.com>
2024-01-31 15:03:47 +1100

    fix: cmdline popup positioning

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -356,9 +356,6 @@ require('lazy').setup({
   {
     'folke/noice.nvim',
     event = 'VeryLazy',
-    opts = {
-      -- add any options here
-    },
     dependencies = {
       -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
       'MunifTanjim/nui.nvim',
@@ -884,6 +881,36 @@ require('catppuccin').setup {
 
 -- [[ Configure noice ]]
 require('noice').setup {
+  views = {
+    cmdline_popup = {
+      position = {
+        row = '42%',
+        col = '50%',
+      },
+      size = {
+        width = 60,
+        height = 'auto',
+      },
+    },
+    popupmenu = {
+      relative = 'editor',
+      position = {
+        row = '60%',
+        col = '50%',
+      },
+      size = {
+        width = 60,
+        height = 10,
+      },
+      border = {
+        style = 'rounded',
+        padding = { 0, 1 },
+      },
+      win_options = {
+        winhighlight = { Normal = 'Normal', FloatBorder = 'DiagnosticInfo' },
+      },
+    },
+  },
   lsp = {
     -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
     override = {
@@ -894,8 +921,8 @@ require('noice').setup {
   },
   -- you can enable a preset for easier configuration
   presets = {
-    bottom_search = true,         -- use a classic bottom cmdline for search
-    command_palette = true,       -- position the cmdline and popupmenu together
+    bottom_search = false,        -- use a classic bottom cmdline for search
+    command_palette = false,      -- position the cmdline and popupmenu together
     long_message_to_split = true, -- long messages will be sent to a split
     inc_rename = false,           -- enables an input dialog for inc-rename.nvim
     lsp_doc_border = false,       -- add a border to hover docs and signature help
