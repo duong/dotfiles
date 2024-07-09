c9344063f5093467160fe396a3412f4f4a6abfa6
Duong Dinh <dd.dinh11@gmail.com>
2024-07-10 09:59:57 +1000

    fix: deprecated signs feature

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -79,12 +79,20 @@ require('lazy').setup({
     opts = {
       -- See `:help gitsigns.txt`
       signs = {
-        add = { hl = 'GitSignsAdd', text = '┃', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
-        change = { hl = 'GitSignsChange', text = '┃', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
-        delete = { hl = 'GitSignsDelete', text = '▁', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-        topdelete = { hl = 'GitSignsDelete', text = '▔', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-        changedelete = { hl = 'GitSignsChange', text = '┃', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
-        untracked = { hl = 'GitSignsUntracked', text = '┃', numhl = 'GitSignsUntrackedNr', linehl = 'GitSignsUntrackedLn' },
+        add = { text = '┃' },
+        change = { text = '┃' },
+        delete = { text = '▁' },
+        topdelete = { text = '▔' },
+        changedelete = { text = '┃' },
+        untracked = { text = '┃' },
+      },
+      signs_staged = {
+        add = { text = '┃' },
+        change = { text = '┃' },
+        delete = { text = '▁' },
+        topdelete = { text = '▔' },
+        changedelete = { text = '┃' },
+        untracked = { text = '┃' },
       },
       on_attach = function(bufnr)
         vim.keymap.set('n', '<leader>hp', require('gitsigns').preview_hunk, { buffer = bufnr, desc = 'Preview git hunk' })
