4a01d0b81a073a13a5b8c22e1f949ee6a5da56e5
Duong Dinh <dd.dinh11@gmail.com>
2024-07-19 14:26:32 +1000

    chore: move nvim-autopairs to plugins

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -112,18 +112,6 @@ require('lazy').setup({
   'ggandor/leap.nvim',
   'rrethy/vim-illuminate',
   {
-    'windwp/nvim-autopairs',
-    -- Optional dependency
-    dependencies = { 'hrsh7th/nvim-cmp' },
-    config = function()
-      require('nvim-autopairs').setup {}
-      -- If you want to automatically add `(` after selecting a function or method
-      local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
-      local cmp = require 'cmp'
-      cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
-    end,
-  },
-  {
     'kevinhwang91/nvim-ufo',
     dependencies = 'kevinhwang91/promise-async',
   },
