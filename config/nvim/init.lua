6293184202b8ac6267ac3cc8e019171db4868289
Duong Dinh <dd.dinh11@gmail.com>
2024-07-20 09:49:40 +1000

    fix: eslint_d

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -76,6 +76,7 @@ require('lazy').setup({
     dependencies = {
       'williamboman/mason.nvim',
       'nvimtools/none-ls.nvim',
+      'nvimtools/none-ls-extras.nvim',
     },
   },
   { 'akinsho/toggleterm.nvim', version = '*', config = true },
@@ -302,7 +303,7 @@ local null_ls = require 'null-ls'
 null_ls.setup {
   sources = {
     null_ls.builtins.formatting.stylua,
-    null_ls.builtins.diagnostics.eslint_d,
+    require 'none-ls.diagnostics.eslint_d',
     null_ls.builtins.completion.spell,
     null_ls.builtins.formatting.prettierd,
     null_ls.builtins.formatting.csharpier,
