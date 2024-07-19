a21e652ac580e4176bea26da02243ca4071e92fe
Duong Dinh <dd.dinh11@gmail.com>
2024-07-19 16:42:38 +1000

    chore: move telescope setup stuff to telescope.lua

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -104,37 +104,6 @@ require 'commands.highlight-yank'
 -- [[ Configure leap ]]
 require('leap').add_default_mappings()
 
--- Enable telescope fzf native, if installed
-pcall(require('telescope').load_extension, 'fzf')
-
--- See `:help telescope.builtin`
-vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = 'Find recently opened files' })
-vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = 'Find existing buffers' })
-vim.keymap.set('n', '<leader>/', function()
-  -- You can pass additional configuration to telescope to change theme, layout, etc.
-  require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-    winblend = 10,
-    previewer = false,
-  })
-end, { desc = '[/] Fuzzily search in current buffer' })
-
--- vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
-vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, { desc = 'Search Files' })
-vim.keymap.set('n', '<leader>fF', function()
-  require('telescope.builtin').find_files { hidden = true }
-end, { desc = 'Search Files (Hidden)' })
-vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags, { desc = 'Search Help' })
-vim.keymap.set('n', '<leader>fw', require('telescope.builtin').grep_string, { desc = 'Search current word' })
-vim.keymap.set('n', '<leader>fW', function()
-  require('telescope.builtin').grep_string { additional_args = { '--hidden' } }
-end, { desc = 'Search current word (Hidden)' })
-vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep, { desc = 'Search by Grep' })
-vim.keymap.set('n', '<leader>fG', function()
-  require('telescope.builtin').live_grep { additional_args = { '--hidden' } }
-end, { desc = 'Search by Grep (Hidden)' })
-vim.keymap.set('n', '<leader>fd', require('telescope.builtin').diagnostics, { desc = 'Search Diagnostics' })
-vim.keymap.set('n', '<leader>fr', require('telescope.builtin').resume, { desc = 'Search Resume' })
-
 -- [[ Configure toggleterm ]]
 require('toggleterm').setup {}
 
--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -1,3 +1,5 @@
+local actions = require 'telescope.actions'
+
 -- Fuzzy Finder (files, lsp, etc)
 return {
   'nvim-telescope/telescope.nvim',
@@ -17,35 +19,66 @@ return {
       end,
     },
   },
-  config = function()
+  opts = {
+    defaults = {
+      path_display = { 'truncate' },
+      sorting_strategy = 'ascending',
+      layout_config = {
+        horizontal = { prompt_position = 'top', preview_width = 0.55 },
+        vertical = { mirror = false },
+        width = 0.87,
+        height = 0.80,
+        preview_cutoff = 120,
+      },
+      mappings = {
+        i = {
+          ['<C-n>'] = actions.cycle_history_next,
+          ['<C-p>'] = actions.cycle_history_prev,
+          ['<C-j>'] = actions.move_selection_next,
+          ['<C-k>'] = actions.move_selection_previous,
+          ['<Tab>'] = actions.move_selection_next,
+          ['<S-Tab>'] = actions.move_selection_previous,
+          ['<C-h>'] = actions.select_horizontal,
+          ['<C-x>'] = actions.delete_buffer,
+        },
+        n = { q = actions.close },
+      },
+    },
+  },
+  config = function(_, opts)
     -- [[ Configure Telescope ]]
     -- See `:help telescope` and `:help telescope.setup()`
-    local actions = require 'telescope.actions'
-    require('telescope').setup {
-      defaults = {
-        path_display = { 'truncate' },
-        sorting_strategy = 'ascending',
-        layout_config = {
-          horizontal = { prompt_position = 'top', preview_width = 0.55 },
-          vertical = { mirror = false },
-          width = 0.87,
-          height = 0.80,
-          preview_cutoff = 120,
-        },
-        mappings = {
-          i = {
-            ['<C-n>'] = actions.cycle_history_next,
-            ['<C-p>'] = actions.cycle_history_prev,
-            ['<C-j>'] = actions.move_selection_next,
-            ['<C-k>'] = actions.move_selection_previous,
-            ['<Tab>'] = actions.move_selection_next,
-            ['<S-Tab>'] = actions.move_selection_previous,
-            ['<C-h>'] = actions.select_horizontal,
-            ['<C-x>'] = actions.delete_buffer,
-          },
-          n = { q = actions.close },
-        },
-      },
-    }
+    require('telescope').setup(opts)
+
+    -- Enable telescope fzf native, if installed
+    pcall(require('telescope').load_extension, 'fzf')
+
+    -- See `:help telescope.builtin`
+    vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = 'Find recently opened files' })
+    vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = 'Find existing buffers' })
+    vim.keymap.set('n', '<leader>/', function()
+      -- You can pass additional configuration to telescope to change theme, layout, etc.
+      require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+        winblend = 10,
+        previewer = false,
+      })
+    end, { desc = '[/] Fuzzily search in current buffer' })
+
+    -- vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
+    vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, { desc = 'Search Files' })
+    vim.keymap.set('n', '<leader>fF', function()
+      require('telescope.builtin').find_files { hidden = true }
+    end, { desc = 'Search Files (Hidden)' })
+    vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags, { desc = 'Search Help' })
+    vim.keymap.set('n', '<leader>fw', require('telescope.builtin').grep_string, { desc = 'Search current word' })
+    vim.keymap.set('n', '<leader>fW', function()
+      require('telescope.builtin').grep_string { additional_args = { '--hidden' } }
+    end, { desc = 'Search current word (Hidden)' })
+    vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep, { desc = 'Search by Grep' })
+    vim.keymap.set('n', '<leader>fG', function()
+      require('telescope.builtin').live_grep { additional_args = { '--hidden' } }
+    end, { desc = 'Search by Grep (Hidden)' })
+    vim.keymap.set('n', '<leader>fd', require('telescope.builtin').diagnostics, { desc = 'Search Diagnostics' })
+    vim.keymap.set('n', '<leader>fr', require('telescope.builtin').resume, { desc = 'Search Resume' })
   end,
 }
