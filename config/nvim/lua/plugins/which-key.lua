7053553e8f564dfacab095f2d8617de27046ef3d
Duong Dinh <dd.dinh11@gmail.com>
2024-07-19 14:04:53 +1000

    chore: move which-key to plugins

---
+++ config/nvim/lua/plugins/which-key.lua
@@ -0,0 +1,77 @@
+-- Useful plugin to show you pending keybinds.
+return {
+  'folke/which-key.nvim',
+  lazy = false,
+  opts = {
+    preset = 'modern',
+  },
+  config = function()
+    -- document existing key chains
+    local wk = require 'which-key'
+    wk.add {
+      { '<leader>c', desc = 'Code' },
+      { '<leader>d', desc = 'Document' },
+      { '<leader>l', desc = 'LSP options' },
+      { '<leader>u', group = 'UI' },
+      { '<leader>uw', '<cmd>set wrap!<CR>', desc = 'Toggle word wrap' },
+      { '<leader>g', group = 'Git options' },
+      { '<leader>gg', '<cmd>lua _lazygit_toggle()<CR>', desc = 'Toggle lazygit' },
+      { '<leader>gl', desc = 'Toggle git line blame' },
+      { '<leader>gd', desc = 'View git diff' },
+      { '<leader>gD', desc = 'View git Diff (?) ' },
+      { '<leader>h', desc = 'More git' },
+      { '<leader>r', desc = 'Rename' },
+      { '<leader>f', desc = 'Find' },
+      { '<leader>w', desc = 'Workspace' },
+      { '<leader>lf', '<cmd>Format<CR>', desc = 'Format buffer' },
+      { '<leader>lo', '<cmd>OrganizeImports<CR>', desc = 'Organize Imports' },
+      { '<leader>n', desc = 'Noice' },
+      {
+        '<leader>nd',
+        function()
+          require('noice').cmd 'dismiss'
+        end,
+        desc = 'Notification dismiss',
+      },
+      {
+        '<leader>nf',
+        function()
+          require('noice').cmd 'telescope'
+        end,
+        desc = 'Notification find',
+      },
+      {
+        '<leader>nl',
+        function()
+          require('noice').cmd 'last'
+        end,
+        desc = 'Notification last',
+      },
+      {
+        '<leader>nh',
+        function()
+          require('noice').cmd 'history'
+        end,
+        desc = 'Notification history',
+      },
+      { '<leader>s', desc = 'Search' },
+      {
+        mode = { 'n' },
+        { '<leader>ss', '<cmd>lua require("spectre").toggle()<CR>', desc = 'Toggle Spectre' },
+        { '<leader>sw', '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', desc = 'Search current word' },
+        { '<leader>sp', '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', desc = 'Search on current file' },
+      },
+      { '<leader>sw', '<esc><cmd>lua require("spectre").open_visual()<CR>', desc = 'Search current word', mode = 'n' },
+      { '<C-b>', '<Cmd>Neotree toggle reveal_force_cwd<CR>', mode = 'n' },
+
+      -- Diagnostic keymaps
+      {
+        mode = { 'n' },
+        { '[d', '<cmd>Lspsaga diagnostic_jump_prev<cr>', desc = 'Go to previous diagnostic message' },
+        { ']d', '<cmd>Lspsaga diagnostic_jump_next<cr>', desc = 'Go to next diagnostic message' },
+        { '<leader>ld', '<cmd>Lspsaga show_line_diagnostics<CR>', desc = 'Open floating diagnostic message' },
+        { '<leader>q', vim.diagnostic.setloclist, desc = 'Open diagnostics list' },
+      },
+    }
+  end,
+}
