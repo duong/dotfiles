41cdd2e837063c36d38569cdf60a2bf248e07572
Duong Dinh <dd.dinh11@gmail.com>
2024-07-19 13:31:53 +1000

    chore: move telescope to plugins

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -131,26 +131,6 @@ require('lazy').setup({
     },
   },
   {
-    'nvim-telescope/telescope.nvim',
-    branch = '0.1.x',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      -- Fuzzy Finder Algorithm which requires local dependencies to be built.
-      -- Only load if `make` is available. Make sure you have the system
-      -- requirements installed.
-      {
-        'nvim-telescope/telescope-fzf-native.nvim',
-        -- NOTE: If you are having trouble with this installation,
-        --       refer to the README for telescope-fzf-native for more instructions.
-        build = 'make',
-        cond = function()
-          return vim.fn.executable 'make' == 1
-        end,
-      },
-    },
-  },
-
-  {
     -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     dependencies = {
@@ -409,36 +389,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 
 -- [[ Configure indent-blankline ]]
 -- require("ibl").setup()
-
--- [[ Configure Telescope ]]
--- See `:help telescope` and `:help telescope.setup()`
-local actions = require 'telescope.actions'
-require('telescope').setup {
-  defaults = {
-    path_display = { 'truncate' },
-    sorting_strategy = 'ascending',
-    layout_config = {
-      horizontal = { prompt_position = 'top', preview_width = 0.55 },
-      vertical = { mirror = false },
-      width = 0.87,
-      height = 0.80,
-      preview_cutoff = 120,
-    },
-    mappings = {
-      i = {
-        ['<C-n>'] = actions.cycle_history_next,
-        ['<C-p>'] = actions.cycle_history_prev,
-        ['<C-j>'] = actions.move_selection_next,
-        ['<C-k>'] = actions.move_selection_previous,
-        ['<Tab>'] = actions.move_selection_next,
-        ['<S-Tab>'] = actions.move_selection_previous,
-        ['<C-h>'] = actions.select_horizontal,
-        ['<C-x>'] = actions.delete_buffer,
-      },
-      n = { q = actions.close },
-    },
-  },
-}
 
 -- [[ Configure leap ]]
 require('leap').add_default_mappings()
---
+++ config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,50 @@
+return {
+  'nvim-telescope/telescope.nvim',
+  branch = '0.1.x',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    -- Fuzzy Finder Algorithm which requires local dependencies to be built.
+    -- Only load if `make` is available. Make sure you have the system
+    -- requirements installed.
+    {
+      'nvim-telescope/telescope-fzf-native.nvim',
+      -- NOTE: If you are having trouble with this installation,
+      --       refer to the README for telescope-fzf-native for more instructions.
+      build = 'make',
+      cond = function()
+        return vim.fn.executable 'make' == 1
+      end,
+    },
+  },
+  config = function()
+    -- [[ Configure Telescope ]]
+    -- See `:help telescope` and `:help telescope.setup()`
+    local actions = require 'telescope.actions'
+    require('telescope').setup {
+      defaults = {
+        path_display = { 'truncate' },
+        sorting_strategy = 'ascending',
+        layout_config = {
+          horizontal = { prompt_position = 'top', preview_width = 0.55 },
+          vertical = { mirror = false },
+          width = 0.87,
+          height = 0.80,
+          preview_cutoff = 120,
+        },
+        mappings = {
+          i = {
+            ['<C-n>'] = actions.cycle_history_next,
+            ['<C-p>'] = actions.cycle_history_prev,
+            ['<C-j>'] = actions.move_selection_next,
+            ['<C-k>'] = actions.move_selection_previous,
+            ['<Tab>'] = actions.move_selection_next,
+            ['<S-Tab>'] = actions.move_selection_previous,
+            ['<C-h>'] = actions.select_horizontal,
+            ['<C-x>'] = actions.delete_buffer,
+          },
+          n = { q = actions.close },
+        },
+      },
+    }
+  end,
+}
