2da0567c15f581a6a13226890516eb22dda0e756
Duong Dinh <dd.dinh11@gmail.com>
2024-07-19 13:29:27 +1000

    chore: move gitsigns to plugins + update deps

---
+++ config/nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,60 @@
+return {
+  -- Adds git related signs to the gutter, as well as utilities for managing changes
+  'lewis6991/gitsigns.nvim',
+  opts = {
+    -- See `:help gitsigns.txt`
+    signs = {
+      add = { text = '┃' },
+      change = { text = '┃' },
+      delete = { text = '▁' },
+      topdelete = { text = '▔' },
+      changedelete = { text = '┃' },
+      untracked = { text = '┃' },
+    },
+    signs_staged = {
+      add = { text = '┃' },
+      change = { text = '┃' },
+      delete = { text = '▁' },
+      topdelete = { text = '▔' },
+      changedelete = { text = '┃' },
+      untracked = { text = '┃' },
+    },
+    on_attach = function(bufnr)
+      vim.keymap.set('n', '<leader>hp', require('gitsigns').preview_hunk, { buffer = bufnr, desc = 'Preview git hunk' })
+
+      -- don't override the built-in and fugitive keymaps
+      local gs = package.loaded.gitsigns
+      vim.keymap.set({ 'n', 'v' }, ']c', function()
+        if vim.wo.diff then
+          return ']c'
+        end
+        vim.schedule(function()
+          gs.next_hunk()
+        end)
+        return '<Ignore>'
+      end, { expr = true, buffer = bufnr, desc = 'Jump to next hunk' })
+      vim.keymap.set({ 'n', 'v' }, '[c', function()
+        if vim.wo.diff then
+          return '[c'
+        end
+        vim.schedule(function()
+          gs.prev_hunk()
+        end)
+        return '<Ignore>'
+      end, { expr = true, buffer = bufnr, desc = 'Jump to previous hunk' })
+
+      local function map(mode, l, r, opts)
+        opts = opts or {}
+        opts.buffer = bufnr
+        vim.keymap.set(mode, l, r, opts)
+      end
+
+      -- Actions
+      map('n', '<leader>gl', gs.toggle_current_line_blame)
+      map('n', '<leader>gd', gs.diffthis)
+      map('n', '<leader>gD', function()
+        gs.diffthis '~'
+      end)
+    end,
+  },
+}
