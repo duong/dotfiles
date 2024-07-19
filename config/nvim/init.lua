c3c2912dd1ffabea8e7489a1b6f33cc38bd16c91
Duong Dinh <dd.dinh11@gmail.com>
2024-07-19 13:43:33 +1000

    chore: move lspsaga to plugins

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -148,49 +148,6 @@ require('lazy').setup({
     lazy = false,
   },
   {
-    'nvimdev/lspsaga.nvim', -- fancy popups and lsp niceties, goto definition and peek
-    config = function()
-      require('lspsaga').setup {
-        debug = false,
-        show_server_name = true,
-        border = 'rounded',
-        devicon = true,
-        lightbulb = {
-          enable = true,
-        },
-        finder = {
-          keys = {
-            open = 'o',
-            vsplit = 's',
-            split = 'i',
-            quit = 'q',
-            scroll_down = '<C-f>',
-            scroll_up = '<C-b>',
-          },
-        },
-        definition = {
-          quit = 'q',
-        },
-        code_action = {
-          keys = {
-            quit = 'q',
-            exec = '<CR>',
-          },
-        },
-        rename = {
-          keys = {
-            quit = '<C-c>',
-            exec = '<CR>',
-          },
-        },
-      }
-    end,
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter', -- optional
-      'nvim-tree/nvim-web-devicons', -- optional
-    },
-  },
-  {
     'JoosepAlviste/nvim-ts-context-commentstring',
   },
   'ggandor/leap.nvim',
---
+++ config/nvim/lua/plugins/lspsaga.lua
@@ -0,0 +1,43 @@
+return {
+  'nvimdev/lspsaga.nvim', -- fancy popups and lsp niceties, goto definition and peek
+  config = function()
+    require('lspsaga').setup {
+      debug = false,
+      show_server_name = true,
+      border = 'rounded',
+      devicon = true,
+      lightbulb = {
+        enable = true,
+      },
+      finder = {
+        keys = {
+          open = 'o',
+          vsplit = 's',
+          split = 'i',
+          quit = 'q',
+          scroll_down = '<C-f>',
+          scroll_up = '<C-b>',
+        },
+      },
+      definition = {
+        quit = 'q',
+      },
+      code_action = {
+        keys = {
+          quit = 'q',
+          exec = '<CR>',
+        },
+      },
+      rename = {
+        keys = {
+          quit = '<C-c>',
+          exec = '<CR>',
+        },
+      },
+    }
+  end,
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter', -- optional
+    'nvim-tree/nvim-web-devicons', -- optional
+  },
+}
