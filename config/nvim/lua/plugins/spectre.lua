8c3dedaf5ee3e526ed39ab049c54ffa3e94c0431
Duong Dinh <dd.dinh11@gmail.com>
2025-03-14 10:14:16 +1100

    fix: spectre creating random -E files on macos

--- config/nvim/lua/plugins/spectre.lua
+++ config/nvim/lua/plugins/spectre.lua
@@ -1,4 +1,18 @@
 return {
   'nvim-pack/nvim-spectre',
   dependencies = 'nvim-lua/plenary.nvim',
+  config = function()
+    require('spectre').setup {
+      replace_engine = {
+        ['sed'] = {
+          cmd = 'sed',
+          args = {
+            '-i',
+            '',
+            '-E',
+          },
+        },
+      },
+    }
+  end,
 }
