55d5ac69216c71ca7f4571e65289610eb3bc3783
Duong Dinh <dd.dinh11@gmail.com>
2025-03-22 18:22:20 +1100

    fix: ensure only use spectre fix on darwin

--- config/nvim/lua/plugins/spectre.lua
+++ config/nvim/lua/plugins/spectre.lua
@@ -2,17 +2,19 @@ return {
   'nvim-pack/nvim-spectre',
   dependencies = 'nvim-lua/plenary.nvim',
   config = function()
-    require('spectre').setup {
-      replace_engine = {
-        ['sed'] = {
-          cmd = 'sed',
-          args = {
-            '-i',
-            '',
-            '-E',
+    if vim.loop.os_uname().sysname == 'Darwin' then
+      require('spectre').setup {
+        replace_engine = {
+          ['sed'] = {
+            cmd = 'sed',
+            args = {
+              '-i',
+              '',
+              '-E',
+            },
           },
         },
-      },
-    }
+      }
+    end
   end,
 }
