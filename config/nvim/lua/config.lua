e335f408f867e724530f01940d1263bb6011608a
Duong Dinh <dd.dinh11@gmail.com>
2025-03-24 13:45:34 +1100

    fix: add fix for broken floating diagnostic

--- config/nvim/lua/config.lua
+++ config/nvim/lua/config.lua
@@ -57,3 +57,8 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- https://github.com/nvimdev/lspsaga.nvim/issues/1520 fixes broken show floating diagnostic
+vim.diagnostic.config {
+  severity_sort = true,
+}
