6125fb726530f6c3623a7986bcc24a19429a49cd
Duong Dinh <dd.dinh11@gmail.com>
2025-08-19 16:06:47 +1000

    feat: add avante
    replace tpope's copilot with the pure lua version so theres no conflicts

---
+++ config/nvim/lua/plugins/copilot-lua.lua
@@ -0,0 +1,47 @@
+return {
+  'zbirenbaum/copilot.lua',
+  cmd = 'Copilot',
+  event = 'InsertEnter',
+  config = function()
+    require('copilot').setup {
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        hide_during_completion = true,
+        debounce = 75,
+        keymap = {
+          accept = '<C-l>',
+          accept_word = false,
+          accept_line = false,
+          next = '<M-]>',
+          prev = '<M-[>',
+          dismiss = '<C-x>',
+        },
+      },
+      panel = {
+        enabled = true,
+        auto_refresh = false,
+        keymap = {
+          jump_prev = '[[',
+          jump_next = ']]',
+          accept = '<CR>',
+          refresh = 'gr',
+          open = '<M-CR>',
+        },
+      },
+      filetypes = {
+        markdown = true, -- Enable for markdown to work with Avante
+        yaml = false,
+        help = false,
+        gitcommit = false,
+        gitrebase = false,
+        hgcommit = false,
+        svn = false,
+        cvs = false,
+        ['.'] = false,
+      },
+      copilot_node_command = 'node', -- Specify path to Node.js if using a version manager
+      server_opts_overrides = {},
+    }
+  end,
+}
--- config/nvim/lua/plugins/neotree.lua
+++ config/nvim/lua/plugins/neotree.lua
@@ -6,17 +6,50 @@ return {
     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
     'MunifTanjim/nui.nvim',
   },
-  opts = {
-    event_handlers = {
-      {
-        event = 'file_opened',
-        handler = function()
-          -- auto close
-          -- vimc.cmd("Neotree close")
-          -- OR
-          require('neo-tree.command').execute { action = 'close' }
-        end,
+  opts = {},
+  config = function()
+    require('neo-tree').setup {
+      filesystem = {
+        commands = {
+          avante_add_files = function(state)
+            local node = state.tree:get_node()
+            local filepath = node:get_id()
+            local relative_path = require('avante.utils').relative_path(filepath)
+
+            local sidebar = require('avante').get()
+
+            local open = sidebar:is_open()
+            -- ensure avante sidebar is open
+            if not open then
+              require('avante.api').ask()
+              sidebar = require('avante').get()
+            end
+
+            sidebar.file_selector:add_selected_file(relative_path)
+
+            -- remove neo tree buffer
+            if not open then
+              sidebar.file_selector:remove_selected_file 'neo-tree filesystem [1]'
+            end
+          end,
+        },
+        window = {
+          mappings = {
+            ['oa'] = 'avante_add_files',
+          },
+        },
       },
-    },
-  },
+      event_handlers = {
+        {
+          event = 'file_opened',
+          handler = function()
+            -- auto close
+            -- vimc.cmd("Neotree close")
+            -- OR
+            require('neo-tree.command').execute { action = 'close' }
+          end,
+        },
+      },
+    }
+  end,
 }
