141e56f35294a39cd55242783d9f8470f5183359
Duong Dinh <dd.dinh11@gmail.com>
2025-08-21 16:57:51 +1000

    fix: show when recording in lualine

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -5,12 +5,41 @@ return {
   -- See `:help lualine.txt`
   opts = {
     options = {
-      icons_enabled = false,
-      -- auto is a special theme. It will automatically load theme for your colorscheme.
-      -- If there's no theme available for your colorscheme then it'll try it's best to generate one.
+      icons_enabled = true,
       theme = 'auto',
       component_separators = '|',
       section_separators = '',
+    },
+    sections = {
+      -- Add the macro recording status in the mode section https://github.com/nvim-lualine/lualine.nvim/issues/1355
+      lualine_a = {
+        function()
+          local reg = vim.fn.reg_recording()
+          -- If a macro is being recorded, show "Recording @<register>"
+          if reg ~= '' then
+            return 'Recording @' .. reg
+          else
+            -- Get the full mode name using nvim_get_mode()
+            local mode = vim.api.nvim_get_mode().mode
+            local mode_map = {
+              n = 'NORMAL',
+              i = 'INSERT',
+              v = 'VISUAL',
+              V = 'V-LINE',
+              ['^V'] = 'V-BLOCK',
+              c = 'COMMAND',
+              R = 'REPLACE',
+              s = 'SELECT',
+              S = 'S-LINE',
+              ['^S'] = 'S-BLOCK',
+              t = 'TERMINAL',
+            }
+
+            -- Return the full mode name
+            return mode_map[mode] or mode:upper()
+          end
+        end,
+      },
     },
   },
 }
