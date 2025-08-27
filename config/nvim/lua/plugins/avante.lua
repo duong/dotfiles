4c96a81b08fbc4d995c184da03b1f36cc4084703
Duong Dinh <dd.dinh11@gmail.com>
2025-08-27 22:57:45 +1000

    feat: use claude-sonnet-4

--- config/nvim/lua/plugins/avante.lua
+++ config/nvim/lua/plugins/avante.lua
@@ -2,8 +2,7 @@ return {
   'yetone/avante.nvim',
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   -- ⚠️ must add this setting! ! !
-  build = vim.fn.has 'win32' ~= 0 and 'powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false' or
-      'make',
+  build = vim.fn.has 'win32' ~= 0 and 'powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false' or 'make',
   event = 'VeryLazy',
   version = false, -- Never set this value to "*"! Never!
   ---@module 'avante'
@@ -13,14 +12,15 @@ return {
     -- for example
     provider = 'copilot',
     -- mode = 'legacy', -- https://github.com/yetone/avante.nvim/issues/2100
-    instructions_file = "avante.md",
+    instructions_file = 'avante.md',
     providers = {
       copilot = {
         endpoint = 'https://api.githubcopilot.com',
-        model = 'gpt-4o',
-        proxy = nil,            -- [protocol://]host[:port] Use this proxy
+        -- model = 'gpt-4o',
+        model = 'claude-sonnet-4',
+        proxy = nil, -- [protocol://]host[:port] Use this proxy
         allow_insecure = false, -- Allow insecure server connections
-        timeout = 30000,        -- Timeout in milliseconds
+        timeout = 30000, -- Timeout in milliseconds
         max_completion_tokens = 8192,
         extra_request_body = {
           temperature = 0.75,
@@ -51,14 +51,14 @@ return {
     'nvim-lua/plenary.nvim',
     'MunifTanjim/nui.nvim',
     --- The below dependencies are optional,
-    'echasnovski/mini.pick',         -- for file_selector provider mini.pick
+    'echasnovski/mini.pick', -- for file_selector provider mini.pick
     'nvim-telescope/telescope.nvim', -- for file_selector provider telescope
-    'hrsh7th/nvim-cmp',              -- autocompletion for avante commands and mentions
-    'ibhagwan/fzf-lua',              -- for file_selector provider fzf
-    'stevearc/dressing.nvim',        -- for input provider dressing
-    'folke/snacks.nvim',             -- for input provider snacks
-    'nvim-tree/nvim-web-devicons',   -- or echasnovski/mini.icons
-    'zbirenbaum/copilot.lua',        -- for providers='copilot'
+    'hrsh7th/nvim-cmp', -- autocompletion for avante commands and mentions
+    'ibhagwan/fzf-lua', -- for file_selector provider fzf
+    'stevearc/dressing.nvim', -- for input provider dressing
+    'folke/snacks.nvim', -- for input provider snacks
+    'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
+    'zbirenbaum/copilot.lua', -- for providers='copilot'
     {
       -- support for image pasting
       'HakonHarnes/img-clip.nvim',
