812a5f24ceace5c558d45cb6aafb02392ac10072
Duong Dinh <dd.dinh11@gmail.com>
2025-08-31 21:25:31 +1000

    feat: add ccc.nvim for css colours

---
+++ config/nvim/lua/plugins/ccc.lua
@@ -0,0 +1,10 @@
+return {
+  'uga-rosa/ccc.nvim',
+  opts = {
+    highlighter = {
+      auto_enable = true,
+      lsp = true,
+      filetypes = { 'css', 'scss', 'html', 'javascript', 'typescript', 'lua' },
+    },
+  },
+}
