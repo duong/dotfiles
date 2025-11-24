996c48ae23b628af88f7f7fcb91eefe7bf44c94a
Duong Dinh <dd.dinh11@gmail.com>
2025-11-24 16:06:27 +1100

    fix: copy relative path function

--- config/nvim/lua/commands/copy-relative-path.lua
+++ config/nvim/lua/commands/copy-relative-path.lua
@@ -1,5 +1,5 @@
 return vim.api.nvim_create_user_command('CopyRelativePath', function()
-  local path = vim.fn.expand '%'
+  local path = vim.fn.fnamemodify(vim.fn.expand '%', ':.')
   vim.fn.setreg('+', path)
   vim.notify('Copied "' .. path .. '" to the clipboard!')
 end, {
